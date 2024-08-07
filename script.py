49b0a599837ae3228101bcf3d96da3acdcee7399
Matthew D <mattdeltano@gmail.com>
2024-08-06 21:29:41 -0500

    add dash interface

--- script.py
+++ script.py
@@ -4,6 +4,29 @@ with open('./data/daily_sales_data_0.csv') as csv_file:
     with open('./data/daily_sales_data_final.csv', mode='w', newline='') as csv_write:
         csv_reader = csv.reader(csv_file, delimiter=',')
         csv_writer = csv.writer(csv_write, delimiter=',')
+        csv_writer.writerow(['sales','date','location'])
+
+        for row in csv_reader:
+            if row[0] == "pink morsel":
+                var1 = float (row[1].removeprefix('$'))
+                var2 = float (row[2])
+                csv_writer.writerow([var1*var2,row[3],row[4]])
+
+with open('./data/daily_sales_data_1.csv') as csv_file:
+    with open('./data/daily_sales_data_final.csv', mode='w', newline='') as csv_write:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_writer = csv.writer(csv_write, delimiter=',')
+
+        for row in csv_reader:
+            if row[0] == "pink morsel":
+                var1 = float (row[1].removeprefix('$'))
+                var2 = float (row[2])
+                csv_writer.writerow([var1*var2,row[3],row[4]])
+
+with open('./data/daily_sales_data_2.csv') as csv_file:
+    with open('./data/daily_sales_data_final.csv', mode='w', newline='') as csv_write:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_writer = csv.writer(csv_write, delimiter=',')
 
         for row in csv_reader:
             if row[0] == "pink morsel":
